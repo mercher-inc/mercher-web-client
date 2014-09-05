2a591878487e580d48474f46386fba538b1ae86f
Dmitry Les <dmitriy.s.les@gmail.com>
2014-09-05 19:41:34 +0300

    access token

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,8 +11,17 @@ angular.module('mercherWebClientApp')
     .controller('MainCtrl', function ($scope, facebook) {
         $scope.login = function () {
             facebook
-                .login({
-                    'scope': 'public_profile,email'
+                .getLoginStatus()
+                .then(function(status){
+                    if (status !== 'connected') {
+                        facebook.login();
+                    }
                 });
         };
+
+        $scope.$on('fb.auth.authResponseChange', function(e, authResponse){
+            if (authResponse.status === 'connected') {
+                console.log(authResponse.authResponse.accessToken);
+            }
+        });
     });
