6ef12f1a7a84ce02cf7dc17d8e4d838667874db1
Dmitry Les <dmitriy.s.les@gmail.com>
2014-09-10 16:08:06 +0300

    navbar

--- app/scripts/directives/mcpage.js
+++ app/scripts/directives/mcpage.js
@@ -4,6 +4,8 @@ angular.module('mercherWebClientApp')
     .directive('mcPage', function () {
         return {
             restrict: 'E',
+            templateUrl: '/views/templates/mcpage.html',
+            transclude: true,
             controller: function() {}
         };
     });
