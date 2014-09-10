2ff9fd8d4f2f532a70e772d4648429d12eca06d3
Dmitry Les <dmitriy.s.les@gmail.com>
2014-09-10 15:18:12 +0300

    image directive

--- app/scripts/directives/mcimage.js
+++ app/scripts/directives/mcimage.js
@@ -1,15 +1,33 @@
 'use strict';
 
 angular.module('mercherWebClientApp')
-    .directive('mcImage', function () {
+    .directive('mcImage', function ($window) {
         return {
             restrict: 'A',
             scope:    {
-                image: '=image',
+                image: '=mcImage',
                 size:  '@size'
             },
-            link:     function postLink(scope, element, attr) {
-                element.attr('src', '/uploads/' + scope.image.key + '/' + scope.image.files[scope.size].mdpi.file);
+            link:     function postLink(scope, element) {
+                scope.size = scope.size || 'm';
+                var devicePixelRatio = $window.devicePixelRatio || 1;
+                var devicePixelRatioName = 'mdpi';
+
+                if (devicePixelRatio > 1) {
+                    devicePixelRatioName = 'hdpi';
+                }
+                if (devicePixelRatio > 1.5) {
+                    devicePixelRatioName = 'xhdpi';
+                }
+                if (devicePixelRatio > 2) {
+                    devicePixelRatioName = 'xxhdpi';
+                }
+                if (devicePixelRatio > 3) {
+                    devicePixelRatioName = 'xxxhdpi';
+                }
+
+                console.log(devicePixelRatio);
+                element.attr('src', '/uploads/' + scope.image.key + '/' + scope.image.files[scope.size][devicePixelRatioName].file);
             }
         };
     });
