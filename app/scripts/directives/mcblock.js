6d06b58bee555888912b3c3b9b0ffd6a7bd4ac2a
Dmitry Les <dmitriy.s.les@gmail.com>
2014-10-10 14:50:08 +0300

    M2-56 Product details page

--- app/scripts/directives/mcblock.js
+++ app/scripts/directives/mcblock.js
@@ -1,8 +1,32 @@
 'use strict';
 
 angular.module('mercherWebClientApp')
-    .directive('mcBlock', function () {
+    .directive('mcBlock', function ($window) {
         return {
-            restrict: 'C'
+            require: '^mcPage',
+            restrict: 'C',
+            link: function(scope, element, attr) {
+                var window = angular.element($window);
+                var type = null;
+                var updateDimentions = function(){
+                    element.css({
+                        height: window.innerHeight()
+                    });
+                    if (element.hasClass('quadratic')) {
+                        element.css({
+                            width: window.innerHeight()
+                        });
+                    } else if (element.hasClass('rectangular')) {
+                        element.css({
+                            width: window.innerHeight() / 2
+                        });
+                    }
+                };
+                updateDimentions();
+                window.bind('resize', updateDimentions);
+            },
+            controller: function($scope){
+
+            }
         };
     });
--- app/scripts/directives/mcpage.js
+++ app/scripts/directives/mcpage.js
@@ -1,8 +1,21 @@
 'use strict';
 
 angular.module('mercherWebClientApp')
-    .directive('mcPage', function () {
+    .directive('mcPage', function ($window) {
         return {
-            restrict: 'C'
+            restrict: 'C',
+            link: function(scope, element) {
+                var window = angular.element($window);
+                var updateHeight = function(){
+                    element.css({
+                        height: window.innerHeight()
+                    });
+                };
+                updateHeight();
+                window.bind('resize', updateHeight);
+            },
+            controller: function($scope){
+
+            }
         };
     });
