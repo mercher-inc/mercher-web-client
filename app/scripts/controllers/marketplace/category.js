a0d97888b787590d2355f58371c002dedd286552
Dmitry Les <dmitriy.s.les@gmail.com>
2014-11-05 18:56:21 +0200

    M2-60 Marketplace page

--- app/scripts/controllers/marketplace/category.js
+++ app/scripts/controllers/marketplace/category.js
@@ -1,5 +1,17 @@
 'use strict';
 
 angular.module('mercherWebClientApp')
-    .controller('MarketplaceCategoryCtrl', function ($scope) {
+    .controller('MarketplaceCategoryCtrl', function ($scope, $stateParams, Categories, Products) {
+        $scope.category = null;
+        $scope.products = [];
+        Categories
+            .get({categoryId: $stateParams.categoryId})
+            .$promise.then(function (category) {
+                $scope.category = category;
+            });
+        Products
+            .queryForCategory({categoryId: $stateParams.categoryId, limit: 100})
+            .$promise.then(function (products) {
+                $scope.products = products.products;
+            });
     });
--- app/scripts/controllers/marketplace/top.js
+++ app/scripts/controllers/marketplace/top.js
@@ -1,5 +1,11 @@
 'use strict';
 
 angular.module('mercherWebClientApp')
-    .controller('MarketplaceTopCtrl', function ($scope) {
+    .controller('MarketplaceTopCtrl', function ($scope, Products) {
+        $scope.products = [];
+        Products
+            .query({limit: 100})
+            .$promise.then(function (products) {
+                $scope.products = products.products;
+            });
     });
